f89afd32a2b59c297edf91d0ce0f7f367b980103
Steffen Vogel <post@steffenvogel.de>
2023-05-05 16:03:03 +0200

    Make repo REUSE compliant

--- datachannel.go
+++ datachannel.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 // Package datachannel implements WebRTC Data Channels
 package datachannel
 
